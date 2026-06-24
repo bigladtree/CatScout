7320877ff05e946ec6785f77362c1dae8f16429f
Chris Cantin <christinacantin1@gmail.com>
2026-06-24 19:56:09 -0400

    Spring-Boot
    set up spring-boot!!!!

---
+++ src/main/java/catscout/catscout/CatscoutApplication.java
@@ -0,0 +1,13 @@
+package catscout.catscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatscoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catscout/catscout/CatscoutApplicationTests.java
@@ -0,0 +1,13 @@
+package catscout.catscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatscoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
